faa8f1df5fc28bb34096809142f56e5c5b41d362
oualid <nassirinassiri@oualid.local>
2025-10-06 22:36:44 +0100

    imigrating database from full sqlite , to prisma with sqlite , and change all the structre of how that we access to our database check folder shared_database folder of prisma

--- services/game-service/server.ts
+++ services/game-service/server.ts
@@ -3,38 +3,34 @@ import cors from 'cors';
 import { WebSocketServer } from 'ws';
 import { createServer } from 'http';
 import Database from './loadSharedDb.js';
+import loadSharedDb from './loadSharedDb.js';
 
 
 const app = express();
 const PORT = process.env.PORT || 3012;
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
-
+const db = loadSharedDb();
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/game', (req, res)=>
-{
-  res.json({status: 'DONE' , service: "game-service"});   
+app.get('/test-db', async (req, res) => {
+  const isConnected = await (await db).testConnection();
+  res.json({ connected: isConnected });
 });
 
+app.get('/users', async (req, res) => {
+  const users = await (await db).getAllUsers();
+  res.json({ users });
+});
 
-wss.on('connection', (ws) => {
-    console.log('New WebSocket connection');
-  
-    ws.on('message', (message) => {
-      console.log('Received:', message.toString());
-    });
-  
-    ws.on('close', () => {
-      console.log('WebSocket connection closed');
-    });
-  });
-
+app.get('/user/:id', async (req, res) => {
+  const user = await (await db).findUserById(parseInt(req.params.id));
+  res.json({ user });
+});
 
 
 server.listen(PORT, () =>
 {
-    console.log("the service of the game is working ");
+    console.log("the service of the game is working !!!!!");
 });
